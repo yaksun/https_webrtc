53dcce9dc84b2a0d0e4cdd1ec3c073f0d021d38c
孙凯 <sunk@gzjp.cn>
2020-11-22 18:10:35 +0800

    修改https

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const fs = require('fs')
 const https = require('https')
+const http = require('http')
+const express = require('express') 
+const serverIndex = require('serve-index')
+
+const app = express(); 
+app.use(serverIndex('./public'))
+app.use(express.static('./public'))
+
+var http_server = http.createServer(app)
+http_server.listen(80,'0.0.0.0')
 
 const options = {
     key:fs.readFileSync('./cert/4809780_www.yaksun.icu.key'),
     cert:fs.readFileSync('./cert/4809780_www.yaksun.icu.pem')
 }
 
-const app = https.createServer(options,function(res,req){
-    res.writeHead(200,{'Content-Type':'text/plain'})
-    res.end('hello')
-})
+var https_server = https.createServer(app)
+https_server.listen(443,'0.0.0.0')
 
-app.listen(443,'0.0.0.0')
+
+console.log('服务器启动中......');
+
