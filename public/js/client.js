f2963d4ecd610e364cad4dc06551c9733d37f82d
孙凯 <sunk@gzjp.cn>
2020-11-22 22:14:43 +0800

    展示本地音频设备

---
+++ public/js/client.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    var audioInput = document.querySelector('#audioInput')
+    var audioOutput = document.querySelector('#audioOutput')
+    var vedioInput = document.querySelector('#vedioInput')
+
+    if(navigator && navigator.mediaDevices){
+        navigator.mediaDevices.enumerateDevices().then(getDevices)
+        .error(handleError)
+    }
+
+    function handleError(){
+
+    }
+
+    function getDevices(deviceInfos){
+        deviceInfos.forEach(function(deviceInfo){
+            var option = document.createElement('option')
+            if(deviceInfo.kind === 'audioinput'){
+                audioInput.appendChild(option)
+            }else if(deviceInfo.kind === 'audiooutput'){
+                audioOutput.appendChild(option)
+            }else if(deviceInfo.kind === 'videoinput'){
+                vedioInput.appendChild(option)
+            }
+        })
+    }
+
+}
